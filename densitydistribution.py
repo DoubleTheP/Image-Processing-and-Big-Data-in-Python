f42d83f89f70cd3039330fb4a0b762a290b77dc5
Physikerphilip <DoubleTheP@users.noreply.github.com>
2019-03-18 21:12:53 +0100

    Update densitydistribution.py
    Works with two half moons

--- densitydistribution.py
+++ densitydistribution.py
@@ -19,35 +19,55 @@ Data = np.concatenate((noisy_moons, checked, cluster_number, assigned), axis=1)
 
 
 start = random.randint(0,n_samples-1)
+cluster_no = 1
 
 
-k = [1]
-while len(k) != 0:
-    checklist = []
-    for i in range(len(Data)):
-        if np.linalg.norm(Data[i][:2]-Data[start][:2]) <= epsilon:
-            checklist.append([np.linalg.norm(Data[i][:2]-Data[start][:2]),i])
-            Data[i][3] = 1
-    Data[start][2] = True
-    if len(checklist) >= minPoint:#core
-       Data[start][4] = "1"
-    elif len(checklist) == 0:
-        Data[start][4] = "3" #outline
+
+def detect_densities(start, cluster_no, Data):
+    k = [1]
+    while len(k) != 0:
+        checklist = []
+        for i in range(len(Data)):
+            if np.linalg.norm(Data[i][:2]-Data[start][:2]) <= epsilon:
+                checklist.append([np.linalg.norm(Data[i][:2]-Data[start][:2]),i])
+                Data[i][3] = cluster_no
+        Data[start][2] = True
+        if len(checklist) >= minPoint:#core
+           Data[start][4] = "1"
+        elif len(checklist) == 0:
+            Data[start][4] = "3" #outline
+        else:
+            Data[start][4] = "2" #border
+
+        k = []
+        zae = 0
+        for i in range(len(Data)):
+            if Data[i][2] == True:
+                zae += 1
+            if Data[i][3] == cluster_no and Data[i][2] == False:
+                k.append(i)
+                start = i
+    print(zae)
+
+    if zae != n_samples:
+        for i in range(len(Data)):
+            if Data[i][3] == False:
+                start = i
+                cluster_no += 1
+                return detect_densities(start, cluster_no, Data)
     else:
-        Data[start][4] = "2" #border
+        return
 
-    k = []
-    for i in range(len(Data)):
-        if Data[i][3] == 1 and Data[i][2] == False:
-            k.append(i)
-            start = i
+detect_densities(start, cluster_no, Data)
+
+print(start)
 
 
 
 for i in range(len(Data)):
-    if Data[i][4] == 1:
+    if Data[i][3] == 1:
     	plt.scatter(Data[i][0], Data[i][1], c="green", s=10)
-    elif Data[i][4] == 2:
+    elif Data[i][3] == 2:
     	plt.scatter(Data[i][0], Data[i][1], c="red", s=10)
     else:
         plt.scatter(Data[i][0], Data[i][1], facecolors='none', edgecolors="black", s=10)
